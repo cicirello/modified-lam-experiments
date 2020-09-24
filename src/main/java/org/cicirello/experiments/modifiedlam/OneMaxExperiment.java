064487cb5641e157236d89a8ef4ba060f4776327
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2020-09-24 16:38:47 -0400

    Update OneMaxExperiment.java

--- src/main/java/org/cicirello/experiments/modifiedlam/OneMaxExperiment.java
+++ src/main/java/org/cicirello/experiments/modifiedlam/OneMaxExperiment.java
@@ -1,5 +1,13 @@
 package org.cicirello.experiments.modifiedlam;
 
+import org.cicirello.search.operators.bits.BitVectorInitializer;
+import org.cicirello.search.operators.bits.DefiniteBitFlipMutation;
+import org.cicirello.search.problems.OneMax;
+import org.cicirello.search.representations.BitVector;
+import org.cicirello.search.sa.ModifiedLam;
+import org.cicirello.search.sa.ModifiedLamOriginal;
+import org.cicirello.search.sa.SimulatedAnnealing;
+
 /**
  * 
  *
@@ -7,5 +15,32 @@ package org.cicirello.experiments.modifiedlam;
 public class OneMaxExperiment {
 	
     public static void main( String[] args ) {
+		
+		final int N = 128;
+		for (int runLength = 1000; runLength <= 1000000; runLength *= 10) {
+			OneMax problem = new OneMax();
+			
+			SimulatedAnnealing<BitVector> sa1 = new SimulatedAnnealing<BitVector>(
+				problem, 
+				new DefiniteBitFlipMutation(5),
+				new BitVectorInitializer(N),
+				new ModifiedLamOriginal()
+			);
+			
+			SimulatedAnnealing<BitVector> sa2 = new SimulatedAnnealing<BitVector>(
+				problem, 
+				new DefiniteBitFlipMutation(5), 
+				new BitVectorInitializer(N),
+				new ModifiedLam()
+			);
+			
+			sa1.optimize(runLength);
+			sa2.optimize(runLength);
+			System.out.printf("%d\t%d\t%d\n",
+				runLength,
+				sa1.getProgressTracker().getCost(), 
+				sa2.getProgressTracker().getCost()
+			);
+		}
     }
 }
